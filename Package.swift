5261f3bbae2242e4d74a1f946d04db7d1099bade
Xabier Sestafe <xabier.sestafe@ironchip.com>
2022-11-22 11:37:43 +0100

    dfsfddsfdsdf

--- Package.swift
+++ Package.swift
@@ -8,6 +8,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
+            
             name: "SimulatorPackage",
             targets: ["IronchipCommonsIos", "LBFraudSDKiOS"]),
     ],
